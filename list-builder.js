4f8515dea39993eb4c2e537c343afa60c94cc4f0
Vucko <ronald.suplina1@gmail.com>
2021-04-18 17:46:30 +0100

    WORKING

--- list-builder.js
+++ list-builder.js
@@ -114,17 +114,17 @@ function validator(event) {
   modalBg.classList.remove("bg-active");
 }
 
-/*BMI*/
-const height = document.querySelector("#height");
-const weight = document.querySelector("#weight");
-const calculate = document.querySelector("#calculate");
-const yourBMI = document.querySelector("#finalBMI");
+// /*BMI*/
+// const height = document.querySelector("#height");
+// const weight = document.querySelector("#weight");
+// const calculate = document.querySelector("#calculate");
+// const yourBMI = document.querySelector("#finalBMI");
 
-calculate.addEventListener("click", () => {
-  if (height.value != "" && weight.value != "") {
-    let finalBMI = (weight.value / (height.value * height.value)) * 10000;
-    yourBMI.innerHTML = `Your BMI Is : <span> ${finalBMI.toFixed(2)} </span>`;
-  } else {
-    yourBMI.innerHTML = `Please Enter Correct Value`;
-  }
-});
+// calculate.addEventListener("click", () => {
+//   if (height.value != "" && weight.value != "") {
+//     let finalBMI = (weight.value / (height.value * height.value)) * 10000;
+//     yourBMI.innerHTML = `Your BMI Is : <span> ${finalBMI.toFixed(2)} </span>`;
+//   } else {
+//     yourBMI.innerHTML = `Please Enter Correct Value`;
+//   }
+// });
